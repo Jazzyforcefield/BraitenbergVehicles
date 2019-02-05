f78ed1c6ce938959f019076e6c233c8d2430a174
Amy Larson <lars1050@umn.edu>
2019-02-05 16:19:12 -0600

    fix: restore to original

--- labs/lab05_gotcha/ground.cc
+++ labs/lab05_gotcha/ground.cc
@@ -14,7 +14,6 @@ Ground::Ground(std::string package) : Delivery(package) {
   std::cout << "Creating a ground package." << std::endl;
   //IncrementCount();
   set_days(5);
-  //days_to_deliver_ = 5;
 }
 
 Ground::~Ground() {
@@ -25,5 +24,5 @@ Ground::~Ground() {
 void Ground::Transport() {
   std::cout << "This is being transported via truck." << std::endl;
   std::cout << "It will take exactly ";
-  //std::cout << days_to_deliver_ << " days." << std::endl;
+  std::cout << days_to_deliver_ << " days." << std::endl;
 }
