74811369d08bd7396937228e23ff72d44717bd04
Michael Ung <mike.ung200@gmail.com>
2019-02-18 12:04:22 -0600

    add(lab07): added lab07

---
+++ labs/lab07_style_doxy/src/mainpage.h
@@ -0,0 +1,15 @@
+// @copyright 2019 3081 class, all rights reserved
+
+#ifndef SRC_MAINPAGE_H_
+#define SRC_MAINPAGE_H_
+
+/*! \mainpage Lab07 Index
+ *
+ * \section intro_sec Introduction
+ *
+ * So this is how Doxygen works... and the INPUT should be .. not ../src since in docs, 
+ * .. = src and ../src DNE.
+ *
+ */
+
+#endif  // SRC_MAINPAGE_H_
