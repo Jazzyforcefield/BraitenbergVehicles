20fdf7e842edf28830b865fc06067cfe0af2968d
Michael Ung <mike.ung200@gmail.com>
2019-02-25 15:52:50 -0600

    robot.cc mod

--- labs/lab09_robots/src/robot.cc
+++ labs/lab09_robots/src/robot.cc
@@ -1,7 +1,14 @@
 #include "robot.h"
 
 	Robot::Robot(int id, double radius, Point origin, double speed) : 
-		id_(id), radius_(radius), origin_(origin), speed_(speed) {}
+		id_(id), radius_(radius), origin_(origin), speed_(speed) {
+		
+		Point position(512 + 200.0 * cos(0), 512 + 200.0 * sint(0));
+		position_ = position;
+		sensor_range_ = 3.0 * radius_;
+		sensor_angle = 2.0;
+		
+	}
 	
 	void Robot::Update(double time) {
 	}
