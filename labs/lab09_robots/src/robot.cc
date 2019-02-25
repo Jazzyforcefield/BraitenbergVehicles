d730e6beb6183d1353ae6a99cff956298de9b9f8
Michael Ung <mike.ung200@gmail.com>
2019-02-25 16:23:55 -0600

    update

--- labs/lab09_robots/src/robot.cc
+++ labs/lab09_robots/src/robot.cc
@@ -1,16 +1,22 @@
+#include <cmath>
+
 #include "robot.h"
 
 	Robot::Robot(int id, double radius, Point origin, double speed) : 
 		id_(id), radius_(radius), origin_(origin), speed_(speed) {
 		
-		Point position(512 + 200.0 * cos(0), 512 + 200.0 * sint(0));
+		Point position(origin_.x_ + speed_ * cos(0), origin_.y_ + speed_ * sin(0));
 		position_ = position;
 		sensor_range_ = 3.0 * radius_;
-		sensor_angle = 2.0;
+		sensor_angle_ = 2.0;
+		direction_ = 0;
 		
 	}
 	
 	void Robot::Update(double time) {
+		double delta_x = origin_.x_ + speed_ * cos(time);
+		double delta_y = origin_.y_ + speed_ * sin(time);
+		direction_ = atan2(delta_x, delta_y);
 	}
 	
 	bool Robot::get_color() {
