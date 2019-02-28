8e02827ea135f1db2285709b3a7ce60f12cbcbe0
dtorban <dtorban@umn.edu>
2019-02-28 10:46:00 -0600

    Update arena.cc

--- project/src/arena.cc
+++ project/src/arena.cc
@@ -43,9 +43,8 @@ Arena::Arena(json_object& arena_object): x_dim_(X_DIM),
   json_array& entities = arena_object["entities"].get<json_array>();
   for (unsigned int f = 0; f < entities.size(); f++) {
     json_object& entity_config = entities[f].get<json_object>();
-    unsigned int type = get_entity_type(
+    EntityType etype = get_entity_type(
       entity_config["type"].get<std::string>());
-    EntityType etype = static_cast<EntityType>(type);
 
     ArenaEntity* entity = NULL;
 
