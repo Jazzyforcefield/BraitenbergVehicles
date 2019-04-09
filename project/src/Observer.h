eaee7ce3c3a87c07d880fee18a6742de6f64fb3f
Michael Ung <mike.ung200@gmail.com>
2019-04-08 21:50:50 -0500

    Observer Pattern with display #15

---
+++ project/src/Observer.h
@@ -0,0 +1,21 @@
+// Copyright 2019, Michael Ung
+
+#ifndef SRC_OBSERVER_H_
+#define SRC_OBSERVER_H_
+
+#include "src/wheel_velocity.h"
+
+NAMESPACE_BEGIN(csci3081);
+
+class Observer {
+ public:
+  virtual ~Observer() {};
+  /**
+   *  @brief Updates the current information in the observer class
+   */
+  virtual void Update(std::vector<WheelVelocity>) {};
+};
+
+NAMESPACE_END(csci3081);
+
+#endif // SRC_OBSERVER_H_
